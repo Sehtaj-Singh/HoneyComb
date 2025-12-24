aecdfec9187166ccef3de0d18bf1bf6ff43bd498
Sehtaj Singh <sehtajm49@gmail.com>
2025-12-24 12:52:09 +0530

    see

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,17 @@
-exports.getIndex = (req, res, next) => {
-  res.render(`pages/index`);
+
+exports.getIndex = (req, res) => {
+  res.render('pages/index', { active: 'home' });
+};
+
+exports.getStore = (req, res) => {
+  res.render('pages/store', { active: 'store' });
+};
+
+exports.getOrders = (req, res) => {
+  res.render('pages/order', { active: 'orders' });
+};
+
+
+exports.getProfile = (req, res) => {
+  res.render('pages/profile', { active: 'profile' });
 };
--- routers/storeRouter.js
+++ routers/storeRouter.js
@@ -6,5 +6,8 @@ const storeRouter = express.Router();
 const storeController = require(`../controllers/storeController`);
 
 storeRouter.get(`/` ,  storeController.getIndex);
+storeRouter.get('/store', storeController.getStore);
+storeRouter.get('/orders', storeController.getOrders);
+storeRouter.get('/profile', storeController.getProfile);
 
 module.exports = storeRouter;
